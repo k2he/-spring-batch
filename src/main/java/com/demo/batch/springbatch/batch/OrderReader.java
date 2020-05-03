54d77d7436493fce9c75602cedbefe25c3cf653c
k2he <he_kai517@hotmail.com>
2020-05-03 19:53:47 -0400

    Add convector to convert String into LocalDateTime

--- src/main/java/com/demo/batch/springbatch/batch/OrderReader.java
+++ src/main/java/com/demo/batch/springbatch/batch/OrderReader.java
@@ -1,10 +1,17 @@
 package com.demo.batch.springbatch.batch;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
@@ -25,11 +32,26 @@ public class OrderReader extends FlatFileItemReader<Order> {
     lineTokenizer.setStrict(false);
     
     BeanWrapperFieldSetMapper<Order> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+    fieldSetMapper.setConversionService(conversionService());
     fieldSetMapper.setTargetType(Order.class);
 
     DefaultLineMapper<Order> defaultLineMapper = new DefaultLineMapper<>();
     defaultLineMapper.setLineTokenizer(lineTokenizer);
     defaultLineMapper.setFieldSetMapper(fieldSetMapper);
     setLineMapper(defaultLineMapper);
+  }
+  
+  public ConversionService conversionService() {
+      DefaultConversionService conversionService = new DefaultConversionService();
+      DefaultConversionService.addDefaultConverters(conversionService);
+      conversionService.addConverter(new Converter<String, LocalDateTime>() {
+          @Override
+          public LocalDateTime convert(String text) {
+            LocalDate date = LocalDate.parse(text);
+            return date.atStartOfDay();
+          }
+      });
+
+      return conversionService;
   }
 }
--- src/main/java/com/demo/batch/springbatch/model/Order.java
+++ src/main/java/com/demo/batch/springbatch/model/Order.java
@@ -1,6 +1,7 @@
 package com.demo.batch.springbatch.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,7 +34,7 @@ public class Order {
 
   private BigDecimal amount;
 
-  private String orderDate;
+  private LocalDateTime orderDate;
 
   private String note;
 
